8edf3eedbc2f0368f019ecb3e98319490b3eb3d8
Oluchi Onyekpa <onyekpaoluchi@gmail.com>
2021-09-17 02:24:43 +0100

    Fixed file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: MAIN_H
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int main(void);
+
+#endif
