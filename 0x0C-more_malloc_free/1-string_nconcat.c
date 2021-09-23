d57f046c216c388780b17385e9adc145ffca7794
Oluchi Onyekpa <onyekpaoluchi@gmail.com>
2021-09-23 20:32:34 +0100

    Added 0x0C-more_malloc_free folder

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,47 +3,47 @@
 #include <stdlib.h>
 
 /**
-* string_nconcat - concatenates two strings.
-* @s1: first string.
-* @s2: second string.
-* @n: number of bytes.
-* Return: pointer to the allocated memory.
-* if malloc fails, status value is equal to 98.
-*/
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: number of bytes.
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *sout;
-unsigned int a1, a2, aout, i;
+	char *sout;
+	unsigned int a1, a2, aout, i;
 
-if (s1 == NULL)
-s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-if (s2 == NULL)
-s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-for (a1 = 0; s1[a1] != '\0'; a1++)
-;
+	for (a1 = 0; s1[a1] != '\0'; a1++)
+		;
 
-for (a2 = 0; s2[a2] != '\0'; a2++)
-;
+	for (a2 = 0; s2[a2] != '\0'; a2++)
+		;
 
-if (n >= a2)
-n = a2;
+	if (n >= a2)
+		n = a2;
 
-aout = a1 + n;
+	aout = a1 + n;
 
-sout = malloc(aout + 1);
+	sout = malloc(aout + 1);
 
-if (sout == NULL)
-return (NULL);
+	if (sout == NULL)
+		return (NULL);
 
-for (i = 0; i < aout; i++)
-if (i < a1)
-sout[i] = s1[i];
-else
-sout[i] = s2[i - a1];
+	for (i = 0; i < aout; i++)
+		if (i < a1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - a1];
 
-sout[i] = '\0';
+	sout[i] = '\0';
 
-return (sout);
+	return (sout);
 }
