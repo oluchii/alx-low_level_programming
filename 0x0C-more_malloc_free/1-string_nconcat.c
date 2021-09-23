cb682b0cf8652035bc4e9bfbd7a9001b7d3b5d2b
Oluchi Onyekpa <onyekpaoluchi@gmail.com>
2021-09-23 22:28:51 +0100

    Modofied 0x0C-more_malloc_free file 5

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 
 /**
-* string_nconcat - Concatenates two strings using at
-*                  most an inputted number of bytes.
-* @s1: The first string.
-* @s2: The second string.
-* @n: The maximum number of bytes of s2 to concatenate to s1.
+* string_nconcat - concatenates two strings
+* @s1: string 1
+* @s2: to be added to end of s1
+* @n: number of bytes to be concatenated
 *
-* Return: If the function fails - NULL.
-*         Otherwise - a pointer to the concatenated space in memory.
+* Return: pointer to new string containg s1 followed by n bytes of s2
+* and null terminated
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *concat;
-unsigned int len = n, index;
+char *ptr;
+unsigned int size1 = 0, size2 = 0, i, j;
 
 if (s1 == NULL)
 s1 = "";
@@ -22,23 +21,29 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-for (index = 0; s1[index]; index++)
-len++;
+while (s1[size1] != '\0')
+size1++;
 
-concat = malloc(sizeof(char) * (len + 1));
+while (s2[size2] != '\0')
+size2++;
 
-if (concat == NULL)
+ptr = malloc(sizeof(char) * (size1 + n + 1));
+
+if (ptr == NULL)
 return (NULL);
 
-len = 0;
+for (i = 0; i < size1; i++)
+ptr[i] = s1[i];
 
-for (index = 0; s1[index]; index++)
-concat[len++] = s1[index];
+if (n >= size2)
+n = size2;
 
-for (index = 0; s2[index] && index < n; index++)
-concat[len++] = s2[index];
+for (j = 0; j < n; j++)
+{
+ptr[i] = s2[j];
+i++;
+}
 
-concat[len] = '\0';
-
-return (concat);
+ptr[i] = '\0';
+return (ptr);
 }
