15aea60b774b873b5881f4c26c9cf2dc87add1a0
Oluchi Onyekpa <onyekpaoluchi@gmail.com>
2021-09-23 21:45:13 +0100

    Modofied 0x0C-more_malloc_free file 5

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,34 +1,47 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
 * string_nconcat - concatenates two strings
-* @s1:stirng 1
-* @s2:string 2
-* @n: n bytesofstring 2
-* Return: pointer
+* @s1: destination string
+* @s2: source string
+* @n: number of bytes from s2 to be copied
+* Return: concatenated string
 */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *ptr, *empt;
-unsigned int i, k;
+unsigned int i, j, size, len1, len2;
+char *ptr;
 
-empt = "";
 if (s1 == NULL)
-s1 = empt;
+s1 = "";
 if (s2 == NULL)
-s2 = empt;
-for (i = 0; s1[i] != '\0'; i++)
-;
-ptr = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
-if (ptr == NULL)
+s2 = "";
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+if (n > len2)
+n = len2;
+size = len1 + n;
+ptr = malloc(sizeof(char) * size + 1);
+if (!ptr)
 return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
+for (i = 0; i < len1; i++)
 ptr[i] = s1[i];
-for (k = 0; s2[k] != '\0' && k < n; k++, i++)
-ptr[i] = s2[k];
-ptr[i] = '\0';
+for (j = 0; j < n; j++, i++)
+ptr[i] = s2[j];
+ptr[i] = 0;
 return (ptr);
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: string s
+* Return: length of string
+*/
+int _strlen(char *s)
+{
+char *p = s;
+
+while (*s)
+s++;
+return (s - p);
 }
