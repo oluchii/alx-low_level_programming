012973c08e0abbf0525cfb8c62c2ff159966a6d3
Oluchi Onyekpa <onyekpaoluchi@gmail.com>
2021-10-12 22:33:44 +0100

    Modified

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,26 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * print_listint_safe - Print a `listint_t` linked list including mem addresses
- * @head: head of linked list
- * Description: Go through the list only once.
- * Return: number of nodes in list. If fails, exit with status 98.
- */
-size_t print_listint_safe(const listint_t *head)
+ * reverse_listint - function to reverse lists
+ * @head: Pointer to a pointer pointing to a struct
+ *
+ * Return: Returns a pointer to the first node of the reversed list
+**/
+
+listint_t *reverse_listint(listint_t **head)
 {
-	const listint_t *current;
-	size_t count;
-	const listint_t *hold;
+listint_t *tmp = *head;
+listint_t *prev = NULL, *next = NULL;
 
-	current = head;
-	if (current == NULL)
-		exit(98);
+while (tmp != NULL)
+{
+next = tmp->next;
+tmp->next = prev;
+prev = tmp;
+tmp = next;
+}
+*head = prev;
 
-	count = 0;
-	while (current != NULL)
-	{
-		hold = current;
-		current = current->next;
-		count++;
-		printf("[%p] %d\n", (void *)hold, hold->n);
-
-		if (hold < current)
-		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			break;
-		}
-	}
-
-	return (count);
+return (*head);
 }
